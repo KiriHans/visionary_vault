36daa4f060d8e266d4da6cdc1eb5c8523246724c
KiriHans <thamorfiredogma3@gmail.com>
2024-05-02 23:04:01 -0500

    Updated Env variables

--- src/env.js
+++ src/env.js
@@ -8,7 +8,17 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
-    POSTGRES_URL: z.string().url(),
+    POSTGRES_URL: z.string().url().startsWith("postgres://"),
+    POSTGRES_PRISMA_URL: z.string().url().startsWith("postgres://"),
+    POSTGRES_URL_NO_SSL: z.string().url().startsWith("postgres://"),
+    POSTGRES_URL_NON_POOLING: z.string().url().startsWith("postgres://"),
+    POSTGRES_USER: z.string(),
+    POSTGRES_HOST: z.string().url(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_DATABASE: z.string(),
+
+    UPLOADTHING_SECRET: z.string(),
+    UPLOADTHING_APP_ID: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -30,6 +40,16 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     POSTGRES_URL: process.env.POSTGRES_URL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
